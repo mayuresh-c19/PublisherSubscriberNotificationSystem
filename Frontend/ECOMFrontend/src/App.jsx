d033d4d2f82e8d9ab143c3ebc70ebc97d17d9bb7
mayuresh-c19 <mayuresh1918@gmail.com>
2024-06-12 20:59:48 +0530

    Added Frontend for Driver Code Purpose

---
+++ Frontend/ECOMFrontend/src/App.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [topicId, setTopicId] = useState('');
+  const [subscriberId, setSubscriberId] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubscribe = async () => {
+    const response = await fetch('http://localhost:3000/subscribe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ topicId, subscriberId }),
+    });
+    const data = await response.text();
+    setMessage(data);
+  };
+
+  const handleNotify = async () => {
+    const response = await fetch('http://localhost:3000/notify', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ topicId }),
+    });
+    const data = await response.json();
+    setMessage(`Notified subscribers: ${data.join(', ')}`);
+  };
+
+  const handleUnsubscribe = async () => {
+    const response = await fetch('http://localhost:3000/unsubscribe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ topicId, subscriberId }),
+    });
+    const data = await response.text();
+    setMessage(data);
+  };
+
+  const [stateData, setStateData] = useState('');
+
+  const handlePrintState = async () => {
+    const response = await fetch('http://localhost:3000/printState');
+    const data = await response.json();
+  
+    let formattedData = 'Current state of topics:\n';
+    for (const [topicId, subscribers] of Object.entries(data)) {
+      formattedData += `Topic: ${topicId}, Subscribers: ${subscribers.join(', ')}\n`;
+    }
+  
+    setStateData(formattedData);
+  };
+
+  return (
+    <>
+      <div className="App">
+        <h1>Publisher Subscriber Notification System</h1>
+        <input
+          type="text"
+          placeholder="Topic ID"
+          value={topicId}
+          onChange={(e) => setTopicId(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Subscriber ID"
+          value={subscriberId}
+          onChange={(e) => setSubscriberId(e.target.value)}
+        />
+        <button onClick={handleSubscribe}>Subscribe</button>
+        <button onClick={handleNotify}>Notify</button>
+        <button onClick={handleUnsubscribe}>Unsubscribe</button>
+        <button onClick={handlePrintState}>Current State</button>
+        <p>{message}</p>
+      </div>
+      <div className='stateData'>
+        <h2>State Data</h2>
+        <pre>{stateData}</pre>
+      </div>
+    </>
+  );
+}
+
+
+export default App;
